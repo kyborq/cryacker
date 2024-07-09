bb63bfe378a36d7f0500e4148ac47f5cb49912df
Konstantin Podyganov <k.podyganov@mail.ru>
2024-07-09 21:53:13 +0300

    feat: cleaned structure and add font

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { App } from "./App.tsx";
+
+import "@fontsource-variable/montserrat";
+
+import "./assets/reset.css";
+import "./assets/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
